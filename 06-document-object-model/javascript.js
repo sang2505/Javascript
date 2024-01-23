8579221aa0d22ecd88942698d5d853e36c34a861
sang2505 <nguyensang.25052004@gmail.com>
2024-01-23 10:38:21 +0700

    Buổi 3

---
+++ 06-document-object-model/javascript.js
@@ -0,0 +1,404 @@
+// //bài 1: dom-intro
+// // Global window object
+// console.log(window);
+
+// // The document object is part of the window object
+// console.dir(window.document);
+
+// // We can access DOM elements directly with properties
+// console.log(document.body);
+// console.log(document.links[0]);
+
+// // We can set properties of elements
+// // document.body.innerHTML = '<h1>Hello from body</h1>';
+
+// // The document object has a ton of methods. One is `document.write()`, which will write something to the document
+// document.write('Hello from JS');
+
+// // We also have methods to select elements more directly
+// document.getElementById('main').innerHTML = '<h1>Hello from main</h1>';
+
+// document.querySelector('#main h1').innerText = 'Hello';
+
+// //bài 2:/02-examining-document-object-properties
+// let output;
+
+// // document.all is deprecated
+// output = document.all;
+// output = document.all[11];
+// output = document.all.length;
+
+// // Everything in the html tags
+// output = document.documentElement;
+
+// // Head and body tags
+// output = document.head;
+// output = document.body;
+
+// // HTMLCollection of everything in head/body
+// output = document.head.children;
+// output = document.body.children;
+
+// // Random properties
+// output = document.doctype;
+// output = document.domain;
+// output = document.URL;
+// output = document.characterSet;
+// output = document.contentType;
+
+// // Get all forms
+// output = document.forms;
+// output = document.forms[0];
+// output = document.forms[0].id;
+// output = document.forms[0].method;
+// output = document.forms[0].action;
+
+// // Change a form id
+// document.forms[0].id = 'new-id';
+
+// // Get all links
+// // output = document.links;
+// // output = document.links[0];
+// // output = document.links[0].href;
+// // output = document.links[0].href = 'https://facebook.com';
+// // output = document.links[0].id = 'google-link';
+// // output = document.links[0].className = 'google-class';
+// // output = document.links[0].classList;
+
+// // Get all images
+// output = document.images;
+// output = document.images[0];
+// output = document.images[0].src;
+
+// // Turn an HTMLCollection into an array
+// const forms = Array.from(document.forms);
+// forms.forEach((form) => console.log(form));
+
+// console.log(output);
+// //bài 3:03-dom-selectors-single-element
+// // document.getElementById()
+
+// console.log(document.getElementById('app-title'));
+
+// // Get attributes
+// console.log(document.getElementById('app-title').id);
+// console.log(document.getElementById('app-title').className);
+// console.log(document.getElementById('app-title').getAttribute('id'));
+
+// // Set attributes
+// document.getElementById('app-title').title = 'Shopping List';
+// document.getElementById('app-title').setAttribute('class', 'title');
+
+// const title = document.getElementById('app-title');
+
+// // Get/change content
+// console.log(title.textContent);
+// title.textContent = 'Hello World';
+// title.innerText = 'Hello Again';
+// title.innerHTML = '<strong>Shopping List</strong>';
+
+// // Change styles
+// title.style.color = 'red';
+// title.style.backgroundColor = 'black';
+// title.style.padding = '10px';
+// title.style.borderRadius = '10px';
+
+// // document.querySelector()
+
+// // Use any CSS selector
+// console.log(document.querySelector('h1'));
+// console.log(document.querySelector('#app-title'));
+// console.log(document.querySelector('.container'));
+// console.log(document.querySelector('input[type="text"]'));
+// console.log(document.querySelector('li:nth-child(2)').innerText);
+
+// const secondItem = document.querySelector('li:nth-child(2)');
+// secondItem.innerText = 'Apple Juice';
+// secondItem.style.color = 'red';
+
+// // Use these methods on other elements
+// const list = document.querySelector('ul');
+// console.log(list);
+// const firstItem = list.querySelector('li');
+// firstItem.style.color = 'blue';
+// //Bài 4:dom-selectors-multiple-elements
+// // querySelectorAll()
+// // Returns a NodeList
+
+// const listItems = document.querySelectorAll('.item');
+
+// // Access elements by index
+// console.log(listItems[1].innerText);
+
+// // Setting a color for specific element
+// listItems[1].style.color = 'red';
+
+// // We can use forEach() on a NodeList
+// listItems.forEach((item, index) => {
+//   item.style.color = 'red';
+
+//   if (index === 1) {
+//     item.remove();
+//   }
+
+//   if (index === 0) {
+//     item.innerHTML = ` Oranges
+//     <button class="remove-item btn-link text-red">
+//       <i class="fa-solid fa-xmark"></i>
+//     </button>`;
+//   }
+// });
+
+// // getElementsByClassName()
+// // Returns an HTMLCollection
+
+// const listItems2 = document.getElementsByClassName('item');
+
+// console.log(listItems2[2].innerText);
+
+// const listItemsArray = Array.from(listItems2);
+
+// listItemsArray.forEach((item) => {
+//   console.log(item.innerText);
+// });
+
+// // getElementsByTagName()
+
+// const listItems3 = document.getElementsByTagName('li');
+// console.log(listItems3[0].innerText);
+// //bài 5:traversing-the -dom-elements
+// let output;
+
+// // Get child elements from a parent
+
+// const parent = document.querySelector('.parent');
+
+// output = parent.children;
+
+// output = parent.children[1].innerText;
+// output = parent.children[1].className;
+// output = parent.children[1].nodeName;
+
+// parent.children[1].innerText = 'Child Two';
+// parent.children[1].style.color = 'red';
+
+// parent.firstElementChild.innerText = 'Child One';
+// parent.lastElementChild.innerText = 'Child Three';
+
+// // Get parent elements from a child
+
+// const child = document.querySelector('.child');
+
+// output = child.parentElement;
+// child.parentElement.style.border = '1px solid #ccc';
+// child.parentElement.style.padding = '10px';
+
+// // Get sibling elements
+
+// const secondItem = document.querySelector('.child:nth-child(2)');
+
+// output = secondItem;
+// output = secondItem.nextElementSibling;
+
+// secondItem.nextElementSibling.style.color = 'green';
+// secondItem.previousElementSibling.style.color = 'orange';
+
+// console.log(output);
+// //bài 6:traversing-the-dom-nodes
+// let output;
+
+// // Get child nodes
+
+// const parent = document.querySelector('.parent');
+
+// output = parent.childNodes;
+// output = parent.childNodes[0].textContent;
+// output = parent.childNodes[0].nodeName;
+// output = parent.childNodes[3].textContent;
+// output = parent.childNodes[3].outerHTML;
+
+// output = parent.childNodes[3].innerText = 'Child One';
+// output = parent.childNodes[5].style.color = 'red';
+
+// output = parent.firstChild;
+// output = parent.lastChild;
+
+// parent.lastChild.textContent = 'Hello';
+
+// // Get parent node
+
+// const child = document.querySelector('.child');
+
+// output = child.parentNode;
+// output = child.parentElement;
+
+// child.parentNode.style.backgroundColor = '#ccc';
+// child.parentNode.style.padding = '10px';
+
+// // Get sibling nodes
+// const secondItem = document.querySelector('.child:nth-child(2)');
+
+// output = secondItem.nextSibling;
+// output = secondItem.previousSibling;
+
+// console.log(output);
+// //bài 7:create-elementss
+// const div = document.createElement('div');
+// div.className = 'my-element';
+// div.id = 'my-element';
+// div.setAttribute('title', 'My Element');
+
+// // div.innerText = 'Hello World';
+
+// const text = document.createTextNode('Hello World');
+// div.appendChild(text);
+
+// // document.body.appendChild(div);
+
+// document.querySelector('ul').appendChild(div);
+// //bài 8:08-create-item-innerHTML-vs-createElement
+// // Quick & Dirty
+// function createListItem(item) {
+//     const li = document.createElement('li');
+  
+//     li.innerHTML = `${item}
+//     <button class="remove-item btn-link text-red">
+//       <i class="fa-solid fa-xmark"></i>
+//     </button>`;
+  
+//     document.querySelector('.items').appendChild(li);
+//   }
+  
+//   // Clean & Performant
+//   function createNewItem(item) {
+//     const li = document.createElement('li');
+//     li.appendChild(document.createTextNode(item));
+  
+//     const button = document.createElement('button');
+//     button.className = 'remove-item btn-link text-red';
+  
+//     const icon = document.createElement('i');
+//     icon.className = 'fa-solid fa-xmark';
+  
+//     button.appendChild(icon);
+//     li.appendChild(button);
+  
+//     document.querySelector('.items').appendChild(li);
+//   }
+  
+//   createListItem('Eggs');
+//   createNewItem('Cheese');
+//   //bài 9:refactor-to-multiple-functions
+//   function createNewItem(item) {
+//     const li = document.createElement('li');
+//     li.appendChild(document.createTextNode(item));
+  
+//     const button = createButton('remove-item btn-link text-red');
+  
+//     li.appendChild(button);
+  
+//     document.querySelector('.items').appendChild(li);
+//   }
+  
+//   function createButton(classes) {
+//     const button = document.createElement('button');
+//     button.className = classes;
+  
+//     const icon = createIcon('fa-solid fa-xmark');
+//     button.appendChild(icon);
+  
+//     return button;
+//   }
+  
+//   function createIcon(classes) {
+//     const icon = document.createElement('i');
+//     icon.className = classes;
+//     return icon;
+//   }
+  
+//   createNewItem('Cheese');
+//   createNewItem('Sauce');
+//   //bài 10:insert-elements
+//   // insertAdjacentElement Example
+// function insertElement() {
+//     const filter = document.querySelector('.filter');
+  
+//     const h1 = document.createElement('h1');
+//     h1.textContent = 'insertAdjacentElement';
+  
+//     filter.insertAdjacentElement('beforebegin', h1);
+//   }
+  
+//   // insertAdjacentText Example
+//   function insertText() {
+//     const item = document.querySelector('li:first-child');
+  
+//     item.insertAdjacentText('beforebegin', 'insertAdjacentText');
+//   }
+  
+//   // insertAdjacentHTML example
+//   function insertHTML() {
+//     const clearBtn = document.querySelector('#clear');
+  
+//     clearBtn.insertAdjacentHTML('afterend', '<h2>insertAdjacentHTML</h2>');
+//   }
+  
+//   // insertBefore Example
+//   function insertBeforeItem() {
+//     const ul = document.querySelector('ul');
+  
+//     const li = document.createElement('li');
+//     li.textContent = 'insertBefore';
+  
+//     const thirdItem = document.querySelector('li:nth-child(3)');
+  
+//     ul.insertBefore(li, thirdItem);
+//   }
+  
+//   insertElement();
+  
+//   /*
+//   <!-- beforebegin -->
+//   <p>
+//     <!-- afterbegin -->
+//     foo
+//     <!-- beforeend -->
+//   </p>
+//   <!-- afterend -->
+//   */
+//  //bài 14:styles-classes
+//  const text = document.querySelector('p');
+// const itemList = document.querySelector('.item-list');
+// const items = itemList.querySelectorAll('li');
+
+// function run() {
+//   // className - Gets a string of all classes
+//   console.log(itemList.className);
+//   // Changing the classes with className
+//   text.className = 'card dark';
+
+//   // classList - Array of classes, which also has methods to add, remove, toggle and replace
+//   console.log(itemList.classList);
+
+//   // We can loop through the classes
+//   itemList.classList.forEach((c) => console.log(c));
+
+//   // Add, remove, toggle, replace
+//   text.classList.add('dark');
+//   text.classList.remove('card');
+//   text.classList.toggle('hidden');
+//   text.classList.replace('card', 'dark');
+
+//   // style property - Add styles to elements
+//   itemList.style.lineHeight = '3';
+
+//   items.forEach((item, index) => {
+//     item.style.color = 'red';
+
+//     if (index === 2) {
+//       item.style.color = 'blue';
+//     }
+//   });
+// }
+
+// document.querySelector('button').onclick = run;
